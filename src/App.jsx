a1d45bf23592f2d449314c1a3e61846bab153b93
take917 <35625934+take917@users.noreply.github.com>
2022-02-02 23:04:33 +0900

    style make a new style

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
-      <p>お元気ですか？</p>
+      <p style={contentStyle}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
